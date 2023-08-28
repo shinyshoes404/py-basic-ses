5f11e29bcf3f68348061891584a66431afd634f9
shinyshoes <shinyshoes404@protonmail.com>
2023-08-27 21:57:47 -0500

    refactored version working, first draft on README, tests written, updated end to end tesst to run on any commit to dev or main

--- src/py_basic_ses/emailing.py
+++ src/py_basic_ses/emailing.py
@@ -1,9 +1,9 @@
 import boto3
-from botocore.exceptions import ClientError
+from py_basic_ses.exceptions import CredError
 import os, platform
 
 class SESSender:
-    def __init__(self,sendto=None, fromaddr=None, message_txt=None, aws_region=None, fromname=None, msgsubject=None, message_html=None):
+    def __init__(self,sendto: str, fromaddr: str, message_txt: str, aws_region: str, fromname: str = None, msgsubject: str = None, message_html: str = None):
         # set instance variables based on what was passed into __init__()
         self.sendto = sendto
         self.fromaddr = fromaddr
@@ -13,27 +13,10 @@ class SESSender:
         self.msgsubject = msgsubject
         self.message_html = message_html
 
-
     
     def ses_validate(self):
         # Returning False indicates an error. Returning True indicates everything was validated.
-
-        # Only the arguments checked below are required.        
-        if self.sendto == None:
-            print("Error: Missing send to address in the SESSender class's __init__() method. This argument is required.")
-            return False
-        if self.fromaddr == None:
-            print("Error: Missing fromaddr in the SESSender class's __init__() method. This argument is required.")
-            return False
-        
-        if self.message_txt == None:
-            print("Error: Missing message_txt in the SESSender class's __init__() method. This argument is required.")
-            return False
-        
-        if self.aws_region == None:
-            print("Error: aws_region is missing in the SESSender class's __init__() method. This argument is required.")
-            return False
-        
+      
         # ---- AWS Credentials Validations ----        
         # Check for environment variables first
         if os.environ.get('AWS_ACCESS_KEY_ID') and os.environ.get('AWS_SECRET_ACCESS_KEY'):
@@ -42,10 +25,9 @@ class SESSender:
         # Since we don't have the environment variables we need present, we will need to use a credentials file
         # Start by determining the OS of the system running this application to determine the expected path to
         # the credentials file.                      
-        # If the OS is not Linux or Windows, print error and stop routine. We are not supporting other operating systems
+        # If the OS is not Linux or Windows, raise exception and stop routine.
         if platform.system() != "Linux" and platform.system() != "Windows":
-            print("\nError: We don't recognize your operating system. py-basic-ses supports Windows or Linux.\n")
-            return False
+            raise OSError("unrecognized operating system. py-basic-ses supports Windows or Linux")
 
         # determine the expected path to the credentials file based on the operating system
         if platform.system() == "Linux":
@@ -55,74 +37,35 @@ class SESSender:
         if platform.system() == "Windows":
             self.credpath = "C:\\Users\\" + os.getlogin() + "\\.aws\\credentials"
 
-        # store some messages to use later
-        cred_instructions_msg = ("\nIn order to authenticate with AWS SES, you have two options:\n"
-                                "  1) Set environment variables of AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY with correct values\n"
-                                "  2) Create a credentials file at " + self.credpath + " containing\n"
-                                "       [default]\n"
-                                "        aws_access_key_id = your-access-key-here\n"
-                                "        aws_secret_access_key = your-secret-access-key-here\n")
-                                
-
-        cred_readme_msg = ("\nFor help setting up IAM users on AWS, retrieving credentials, and setting policies, "
-                        "checkout the README for py-basic-ses at \nhttps://github.com/shinyshoes404/py-basic-ses\n")
-
         
-        # does the path exist?
-        if os.path.exists(self.credpath) == False:
-            # if not, print a helpful error message, and bail out of this routine.
-            print("\nError: There is a problem with your AWS credentials.")
-            print(cred_instructions_msg)
-            print(cred_readme_msg)
-            return False
+        # since no environment variables, does the cred file path exist? If not, raise an exception
+        if not os.path.isfile(self.credpath):
+            raise CredError(f'missing aws ses credentials, no env vars or file at {self.credpath}, for help setting up aws ses credentials see https://github.com/shinyshoes404/py-basic-ses#readme')
+
         else:
-            # is credentials a file?
-            if os.path.isfile(self.credpath) == False:
-                # if not, print a helpful error message and bail out of this routine.
-                print("\nError: There is a problem with your AWS credentials.")
-                print(self.credpath + " is not a file.")
-                print(cred_instructions_msg)
-                print(cred_readme_msg)
-                return False
+            # do we have read access on the credentials file?
+            if os.access(self.credpath, os.R_OK) == False:
+                raise CredError('cannot access credentials file, the file exists and is in the correct location, but this application does not have permission to read, for help setting up aws ses credentials see https://github.com/shinyshoes404/py-basic-ses#readme')
 
             else:
-                # do we have read access on the credentials file?
-                if os.access(self.credpath, os.R_OK) == False:
-                    # if not, print a helpful error message and bail out of this routine.
-                    print("\nError: Your credentials file exists, and is in the right place, but you don't have access to read its contents. Please fix this permissions issue.")
-                    print(cred_instructions_msg)
-                    print(cred_readme_msg)
-                    return False
+                # Read the contents of the file and look for aws_access_key_id and aws_secret_access_key
+                # Remove any spaces in our string variable to prep for the .find() method.
+                # In the credentials file, there can be as many spaces as you want between the key = value, but we need
+                # to make sure that the key and = are present. If they are not, then the boto3 library will throw an error
+                with open(self.credpath, "r") as cred_file:
+                    cred_content = cred_file.read().replace(" ","")
+                    
+                # look for aws_access_key_id= and aws_secret_access_key=
+                if cred_content.find("aws_access_key_id=") != -1 and cred_content.find("aws_secret_access_key=") != -1 and cred_content.find("[default]") != -1:
+                    return True
                 else:
-                    # Read the contents of the file and look for aws_access_key_id and aws_secret_access_key
-                    # Remove any spaces in our string variable to prep for the .find() method.
-                    # In the credentials file, there can be as many spaces as you want between the key = value, but we need
-                    # to make sure that the key and = are present. If they are not, then the boto3 library will throw an error
-                    with open(self.credpath, "r") as cred_file:
-                        cred_content = cred_file.read().replace(" ","")
-                        
-                    # look for aws_access_key_id= and aws_secret_access_key=
-                    if cred_content.find("aws_access_key_id=") != -1 and cred_content.find("aws_secret_access_key=") != -1 and cred_content.find("[default]") != -1:
-                        return True
-                    else:
-                        print("\nError: Your credentials file is missing something we need.")
-                        print("Make sure your file contains all of the required items.")
-                        print(cred_instructions_msg)
-                        print(cred_readme_msg)
-                        return False
+                    raise CredError('malformed credentials file, for help setting up aws ses credentials see https://github.com/shinyshoes404/py-basic-ses#readme')
         
         
 
-    def send_email(self):
-
+    def send_email(self) -> str:
         # make sure we have all of the required parameters before attempting to send an email
-        try:
-            if self.ses_validate() == False:
-                return False
-        except Exception as e:
-            print("unexpected exception")
-            print(e)
-            return False
+        self.ses_validate()
 
         # Replace sender@example.com with your "From" address.
         # This address must be verified with Amazon SES.
@@ -157,18 +100,12 @@ class SESSender:
         # The character encoding for the email.
         self.CHARSET = "UTF-8"
 
-        try:
-            # Create a new SES resource and specify a region.
-            self.client = boto3.client('ses',region_name=self.AWS_REGION)
-        except Exception as e:
-            print("unexpected exception")
-            print(e)
-            return False
+        # Create a new SES resource and specify a region.
+        self.client = boto3.client('ses',region_name=self.AWS_REGION)
 
-        # Try to send the email.
-        try:
-            #Provide the contents of the email.
-            response = self.client.send_email(
+
+        #Provide the contents of the email.
+        response = self.client.send_email(
                 Destination={
                     'ToAddresses': [
                         self.RECIPIENT,
@@ -191,20 +128,10 @@ class SESSender:
                     },
                 },
                 Source=self.SENDER,
-                
+            
             )
-        # Display an error if something goes wrong.	
-        except ClientError as e:
-            print("client error")
-            print(e.response['Error']['Message'])
-            return False
-        except Exception as e:
-            print("unexpected exception")
-            print(e)
-            return False
-        else:
-            print("Email sent! Message ID:"),
-            print(response['MessageId'])
-            return True
+
+
+        return response['MessageId']
 
     
